2df7229f1db651a098d6fd2c0292bcb45d6d1e17
Anton <toniq133@bk.ru>
2023-04-05 17:59:35 +0300

    first commit

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+    public FormDate birthday;
+    public boolean subscription;
+}
